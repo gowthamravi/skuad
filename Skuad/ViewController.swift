ce1610ff2fe0e850fb149cfae3310e96fc0d2235
Gowtham Ravikumar <gowtham.ravikumar@news.co.uk>
2020-10-13 19:10:53 +0530

    initial commit

---
+++ Skuad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skuad
+//
+//  Created by Ravikumar, Gowtham on 10/13/20.
+//  Copyright © 2020 Ravikumar, Gowtham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
